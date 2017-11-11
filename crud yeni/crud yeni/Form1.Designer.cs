b4c5092011f9cb122f9d2b7be44725dfbb497635
AnarAbishov <mr.avinar@mail.ru>
2017-11-11 04:37:42 +0400

    Add files via upload

---
+++ crud yeni/crud yeni/Form1.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace crud_yeni
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtAge = new System.Windows.Forms.TextBox();
+            this.txtCity = new System.Windows.Forms.TextBox();
+            this.cmbxGender = new System.Windows.Forms.ComboBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.dgvStudent = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvStudent)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(22, 90);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(33, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Age";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(22, 140);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(31, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "City";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(22, 184);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(56, 17);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Gender";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(88, 44);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(171, 22);
+            this.txtName.TabIndex = 4;
+            // 
+            // txtAge
+            // 
+            this.txtAge.Location = new System.Drawing.Point(88, 85);
+            this.txtAge.Name = "txtAge";
+            this.txtAge.Size = new System.Drawing.Size(171, 22);
+            this.txtAge.TabIndex = 5;
+            // 
+            // txtCity
+            // 
+            this.txtCity.Location = new System.Drawing.Point(88, 136);
+            this.txtCity.Name = "txtCity";
+            this.txtCity.Size = new System.Drawing.Size(171, 22);
+            this.txtCity.TabIndex = 6;
+            // 
+            // cmbxGender
+            // 
+            this.cmbxGender.FormattingEnabled = true;
+            this.cmbxGender.Location = new System.Drawing.Point(88, 180);
+            this.cmbxGender.Name = "cmbxGender";
+            this.cmbxGender.Size = new System.Drawing.Size(171, 24);
+            this.cmbxGender.TabIndex = 7;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.btnAdd.Location = new System.Drawing.Point(321, 27);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(70, 40);
+            this.btnAdd.TabIndex = 8;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = false;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.BackColor = System.Drawing.Color.Yellow;
+            this.btnUpdate.Location = new System.Drawing.Point(321, 90);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(70, 40);
+            this.btnUpdate.TabIndex = 9;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = false;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.BackColor = System.Drawing.Color.Red;
+            this.btnDelete.Location = new System.Drawing.Point(321, 164);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(70, 40);
+            this.btnDelete.TabIndex = 10;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = false;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // dgvStudent
+            // 
+            this.dgvStudent.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvStudent.Location = new System.Drawing.Point(12, 231);
+            this.dgvStudent.Name = "dgvStudent";
+            this.dgvStudent.RowTemplate.Height = 24;
+            this.dgvStudent.Size = new System.Drawing.Size(616, 234);
+            this.dgvStudent.TabIndex = 11;
+            this.dgvStudent.RowHeaderMouseDoubleClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.dgvStudent_RowHeaderMouseDoubleClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(635, 477);
+            this.Controls.Add(this.dgvStudent);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.cmbxGender);
+            this.Controls.Add(this.txtCity);
+            this.Controls.Add(this.txtAge);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvStudent)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtAge;
+        private System.Windows.Forms.TextBox txtCity;
+        private System.Windows.Forms.ComboBox cmbxGender;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.DataGridView dgvStudent;
+    }
+}
+
---
+++ crud yeni/crud yeni/Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using crud_yeni.Models;
+
+namespace crud_yeni
+{
+    public partial class Form1 : Form
+    {
+        private StudentInformationEntities db = new StudentInformationEntities();
+        private StudentDetails selected_stdnt;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            Display();
+            dgvStudent.Columns[0].Visible = false;
+        }
+        private void Display()
+        {
+            dgvStudent.DataSource = db.StudentDetails.Select(s => new {s.Id, s.Name, s.Age, s.City, s.Gender}).ToList();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cmbxGender.Items.Add("Male");
+            cmbxGender.Items.Add("Female");
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            
+            if (txtName.Text == "" && txtAge.Text == "" && txtCity.Text == "")
+            {
+                MessageBox.Show("Zehmet olmasa xanalari doldurun");
+            }
+            else
+            {
+                StudentDetails student = new StudentDetails();
+                student.Name = txtName.Text;
+                student.Age = Convert.ToInt32(txtAge.Text);
+                student.City = txtCity.Text;
+                student.Gender = cmbxGender.SelectedItem.ToString();
+
+                db.StudentDetails.Add(student);
+                db.SaveChanges();
+                reset();
+            }
+        }
+
+
+        private void reset()
+        {
+            Display();
+            txtName.Clear();
+            txtAge.Clear();
+            txtCity.Clear();
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            if (txtName.Text == "" && txtAge.Text == "" && txtCity.Text == "")
+            {
+                MessageBox.Show("Zehmet olmasa deyishmek istediyiniz Row-u sechin");
+            }
+            else
+            {
+                selected_stdnt.Name = txtName.Text;
+                selected_stdnt.Age = Convert.ToInt32(txtAge.Text);
+                selected_stdnt.City = txtCity.Text;
+                selected_stdnt.Gender = cmbxGender.SelectedItem.ToString();
+                db.SaveChanges();
+                reset();
+            }
+           
+        }
+
+        private void dgvStudent_RowHeaderMouseDoubleClick(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            int id = Convert.ToInt32(dgvStudent.Rows[e.RowIndex].Cells[0].Value.ToString());
+            selected_stdnt = db.StudentDetails.Find(id);
+            txtName.Text = selected_stdnt.Name;
+            txtAge.Text = Convert.ToString(selected_stdnt.Age);
+            txtCity.Text = selected_stdnt.City;
+            cmbxGender.SelectedItem = selected_stdnt.Gender;
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            DialogResult result = MessageBox.Show("Silmek istirsiz", "Silmek", MessageBoxButtons.YesNo);
+            if (result==DialogResult.Yes)
+            {
+                if (txtName.Text == "" && txtAge.Text == "" && txtCity.Text == "")
+                {
+                    MessageBox.Show("Zehmet olmasa silmek uchun Row sechin");
+                }
+                else
+                {
+                    db.StudentDetails.Remove(selected_stdnt);
+                    db.SaveChanges();
+                    reset();
+                }
+               
+            }
+        }
+    }
+}
